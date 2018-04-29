e1b9190aa3f290037fa44ca220fd59c6045842db
rayraib <binitarai11@gmail.com>
2018-04-29 03:35:59 +0000

    remove print statement

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -11,11 +11,13 @@ import sys
 def get_content(emp_id):
     ''' make API requests call to get todo lists and name for emp_id'''
 
+# create urls to make requests to
     todos_url = "https://jsonplaceholder.typicode.com/todos?userId={}"\
                 .format(emp_id)
     user_url = "https://jsonplaceholder.typicode.com/users?id={}"\
                .format(emp_id)
 
+# make requests
     todo_r = requests.get(todos_url)
     user_r = requests.get(user_url)
 
@@ -23,19 +25,21 @@ def get_content(emp_id):
     user_name = user_json[0]['name']
     user_id = user_json[0]['id']
 
+# create required variables
     todo_data = todo_r.json()
     file_name = "{}.json".format(emp_id)
     format_ = []
     f_dict = {}
 
+# create the data to write to JSON file in desired format
     for item in todo_data:
         f_dict["task"] = item["title"]
         f_dict["completed"] = item["completed"]
         f_dict["username"] = user_name
         format_.append(f_dict)
     final_format = {user_id: format_}
-    print(final_format)
 
+# write the data to JSON file
     with open(file_name, 'w', encoding="utf8") as f:
         f.write(json.dumps(final_format))
 
