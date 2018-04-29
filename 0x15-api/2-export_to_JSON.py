8583997abbc7e76246f91c55efc53a7895d30be5
rayraib <binitarai11@gmail.com>
2018-04-29 00:23:48 +0000

    write to json file

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+''' Python script that, using a REST API, for a given employee ID,
+    returns information about his/her TODO list progress.
+    and export the data to JSON file
+'''
+import json
+import requests
+import sys
+
+
+def get_content(emp_id):
+    ''' make API requests call to get todo lists and name for emp_id'''
+
+    todos_url = "https://jsonplaceholder.typicode.com/todos?userId={}"\
+                .format(emp_id)
+    user_url = "https://jsonplaceholder.typicode.com/users?id={}"\
+               .format(emp_id)
+
+    todo_r = requests.get(todos_url)
+    user_r = requests.get(user_url)
+
+    user_json = user_r.json()
+    user_name = user_json[0]['name']
+    user_id = user_json[0]['id']
+
+    todo_data = todo_r.json()
+    file_name = "{}.json".format(emp_id)
+    format_ = []
+    f_dict = {}
+
+    for item in todo_data:
+        f_dict["task"] = item["title"]
+        f_dict["completed"] = item["completed"]
+        f_dict["username"] = user_name
+        format_.append(f_dict)
+    final_format = {user_id: format_}
+    print(final_format)
+
+    with open(file_name, 'w', encoding="utf8") as f:
+        f.write(json.dumps(final_format))
+
+
+if __name__ == "__main__":
+    emp_id = sys.argv[1]
+    get_content(emp_id)
