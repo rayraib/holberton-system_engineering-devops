096507bff8031f2c0224f60ec6dbf5d14f7c80e4
rayraib <binitarai11@gmail.com>
2018-05-04 03:30:26 +0000

    replace for loop with recurision

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -10,31 +10,40 @@ def recurse(subreddit, hot_list=[]):
         'User-Agent': 'My User Agent 1.0'
     }
 
+    hot_list = query_recursively(url, hot_list, headers, subreddit)
+    if len(hot_list) == 0:
+        return None
+    return hot_list
+
+
+def query_recursively(url, hot_list, headers, subreddit):
+    ''' make recursive call to obtain all hot posts for a subreddit '''
     r = requests.get(url, headers=headers, allow_redirects=False)
     if r.status_code != 200:
-        return None
+        return hot_list
+
+    content = r.json()
+    data = content['data']
+    dist = data['dist']
+    children = data['children']
+    after = data['after']
+    hot_list = get_children(hot_list, children, 0, dist)
+    if after is None:
+        return hot_list
+
+    url = 'https://api.reddit.com/r/{}/hot/?after={}'\
+          .format((subreddit), after)
     hot_list = query_recursively(url, hot_list, headers, subreddit)
     return hot_list
 
 
-def query_recursively(url, result, headers, subreddit):
-    ''' make recursive call to obtain all hot posts for a subreddit '''
-    r = requests.get(url, headers=headers, allow_redirects=False)
-    if r.status_code != 200:
-        return result
-
-    content = r.json()
-    data = content['data']
-    children = data['children']
-    after = data['after']
-    if after is None:
-        return result
-
-    for idx, item in enumerate(children):
-        title = item['data']['title']
-        result.append(title)
-
-    url = 'https://api.reddit.com/r/{}/hot/?after={}'\
-          .format((subreddit), after)
-    result = query_recursively(url, result, headers, subreddit)
-    return result
+def get_children(hot_list, children, count, dist):
+    ''' get the title of each hot post'''
+    if count < dist:
+        title = children[count]['data']['title']
+        hot_list.append(title)
+        count += 1
+        hot_list = get_children(hot_list, children, count, dist)
+        return (hot_list)
+    else:
+        return hot_list
