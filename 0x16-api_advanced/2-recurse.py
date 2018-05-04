aa40474d7672c1adb31839df747005ec9ac74413
rayraib <binitarai11@gmail.com>
2018-05-04 04:04:27 +0000

    call the recurse function recursively and remove the second recursive function

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -3,21 +3,38 @@
 import requests
 
 
-def recurse(subreddit, hot_list=[]):
+def recurse(subreddit, hot_list=[], after=""):
     ''' Query the subreddit recursively for hot posts'''
-    url = 'https://api.reddit.com/r/{}/hot/'.format(subreddit)
-    headers = {
-        'User-Agent': 'My User Agent 1.0'
-    }
 
-    hot_list = query_recursively(url, hot_list, headers, subreddit)
+    url = 'https://api.reddit.com/r/{}/hot/?after={}'\
+          .format((subreddit), after)
+    headers = {
+                'User-Agent': 'My User Agent 1.0'
+              }
+
+    r = requests.get(url, headers=headers, allow_redirects=False)
+    if r.status_code != 200:
+        return None 
+    content = r.json()
+    data = content['data']
+    dist = data['dist']
+    children = data['children']
+    after = data['after']
+    hot_list = get_children(hot_list, children, 0, dist)
+    if after is None:
+        return hot_list
+
+    url = 'https://api.reddit.com/r/{}/hot/?after={}'\
+          .format((subreddit), after)
+    hot_list = recurse(subreddit, hot_list, after)
     if len(hot_list) == 0:
         return None
     return hot_list
 
 
+'''
 def query_recursively(url, hot_list, headers, subreddit):
-    ''' make recursive call to obtain all hot posts for a subreddit '''
+
     r = requests.get(url, headers=headers, allow_redirects=False)
     if r.status_code != 200:
         return hot_list
@@ -35,10 +52,12 @@ def query_recursively(url, hot_list, headers, subreddit):
           .format((subreddit), after)
     hot_list = query_recursively(url, hot_list, headers, subreddit)
     return hot_list
+'''
 
 
 def get_children(hot_list, children, count, dist):
     ''' get the title of each hot post'''
+
     if count < dist:
         title = children[count]['data']['title']
         hot_list.append(title)
