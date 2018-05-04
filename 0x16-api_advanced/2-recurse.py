7bd0340851ef3b6513cdfad5f6cfb52135410c9d
rayraib <binitarai11@gmail.com>
2018-05-04 04:13:48 +0000

    comment

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -13,51 +13,39 @@ def recurse(subreddit, hot_list=[], after=""):
               }
 
     r = requests.get(url, headers=headers, allow_redirects=False)
+    # check if invalid subreddit name
     if r.status_code != 200:
-        return None 
+        return None
+
+    # convert and parse json data for the list of posts
     content = r.json()
     data = content['data']
     dist = data['dist']
     children = data['children']
     after = data['after']
+
+    # call get_children that will find the title
     hot_list = get_children(hot_list, children, 0, dist)
+
+    # if the current page it last page
     if after is None:
         return hot_list
 
+    # construct new url with the after value to get next page
     url = 'https://api.reddit.com/r/{}/hot/?after={}'\
           .format((subreddit), after)
     hot_list = recurse(subreddit, hot_list, after)
+
+    # if page contained no posts
     if len(hot_list) == 0:
         return None
     return hot_list
 
 
-'''
-def query_recursively(url, hot_list, headers, subreddit):
-
-    r = requests.get(url, headers=headers, allow_redirects=False)
-    if r.status_code != 200:
-        return hot_list
-
-    content = r.json()
-    data = content['data']
-    dist = data['dist']
-    children = data['children']
-    after = data['after']
-    hot_list = get_children(hot_list, children, 0, dist)
-    if after is None:
-        return hot_list
-
-    url = 'https://api.reddit.com/r/{}/hot/?after={}'\
-          .format((subreddit), after)
-    hot_list = query_recursively(url, hot_list, headers, subreddit)
-    return hot_list
-'''
-
-
 def get_children(hot_list, children, count, dist):
     ''' get the title of each hot post'''
 
+    # retrieve title if there is content
     if count < dist:
         title = children[count]['data']['title']
         hot_list.append(title)
